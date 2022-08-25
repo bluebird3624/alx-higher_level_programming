7bc58211deb32b7f96f24940a8848342969b005a
Koech Roy <koechroy06@gmail.com>
2022-08-25 20:26:44 +0000

    0. Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
+import sys
 from add_0 import add
 
-a = 1
-b = 2
+"""Importing and using a module
 
-print(f"{a} + {b} = {add(a,b)}")
+    Args:
+        a : first integer
+        b : second integer
+
+    Returns:
+        a print of the operations variables and unswears
+
+"""
+if __name__ == "__main__":
+    a = 1
+    b = 2
+
+    print(f"{a} + {b} = {add(a,b)}")
+
+    sys.exit(0)
