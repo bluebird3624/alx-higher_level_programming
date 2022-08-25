c2cba624b81092c92278c8a9606895dd31481973
Koech Roy <koechroy06@gmail.com>
2022-08-25 20:48:54 +0000

    0. Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import sys
 from add_0 import add
 
 if __name__ == "__main__":
+    """
+     Prints the result of the addition between two numbers
+    """
 
     a = 1
     b = 2
