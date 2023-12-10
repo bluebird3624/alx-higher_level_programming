4f5802222b95dd7756ed151d1633a5b014b5085d
Codesmth <reyclamar06@gmail.com>
2023-12-10 12:25:41 +0000

    0 correction

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new = [[i ** 2 for i in j]for j in matrix]
-    return new
+    return  map(lambda row:list(map(lambda element:element**2,row)),matrix)
