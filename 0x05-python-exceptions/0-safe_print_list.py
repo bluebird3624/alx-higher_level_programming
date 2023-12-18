d5224556ad59efc2210b10b86d573d48189e1e4f
Codesmth <reyclamar06@gmail.com>
2023-12-18 21:06:28 +0000

    exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-    except Exception:
-        print("{} is not an integer".format(value))
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in range(0,x):
+        try:
+            print("{:d}".format(my_list[i]),end="")
+        except Exception:
+            print('')
+            return count
+        finally:
+            count = count + 1
+    print('')
+    return count
